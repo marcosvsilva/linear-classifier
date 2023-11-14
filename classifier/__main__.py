805965d026acf308675336c537d3fdd1c91e84f8
marcos v <marcos.v.silva@live.com>
2023-11-14 23:29:18 +0000

    feat: add noteboks to visualize decision boundary of linear classifiers

--- classifier/__main__.py
+++ classifier/__main__.py
@@ -1,6 +1,13 @@
-from src.classifiers import tracking
+from src.classifiers import projects
 
 if __name__ == "__main__":
-  print('Hello World!!')
-  print('Running tracking classifier!!')
-  tracking()
+  print("Starting classifiers")
+
+  # print("Running animals classifier!!")
+  # tracking()
+
+  # print("Running tracking classifier!!")
+  # tracking()
+
+  print("Running projects classifier!!")
+  projects()
