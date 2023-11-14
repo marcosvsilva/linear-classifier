677d9f4328ebfe1f270401041159d21e28d650eb
marcos v <marcos.v.silva@live.com>
2023-11-14 00:01:00 +0000

    refactor: vars names patterns

--- classifier/classifiers/animals.py
+++ classifier/classifiers/animals.py
@@ -13,11 +13,11 @@ dog1 = [0, 1, 1]
 dog2 = [1, 0, 1]
 dog3 = [1, 1, 1]
 
-data = [pig1, pig2, pig3, dog1, dog2, dog3]
-labels = [1, 1, 1, 0, 0, 0]
+train_x = [pig1, pig2, pig3, dog1, dog2, dog3]
+train_y = [1, 1, 1, 0, 0, 0]
 
 model = LinearSVC()
-model.fit(data, labels)
+model.fit(train_x, train_y)
 
 test = [1, 1, 1]
 print(model.predict([test]))
